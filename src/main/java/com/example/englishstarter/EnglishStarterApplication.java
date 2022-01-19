d5833c8aa40b69236ff3bd51cdfb7f7091f808eb
WenDeHor <WenDeHor>
2022-01-19 20:01:47 +0200

    1.First commit

---
+++ src/main/java/com/example/englishstarter/EnglishStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/englishstarter/EnglishStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.englishstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
